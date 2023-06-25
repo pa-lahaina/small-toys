fb8603929d4e07362416e3e9815864d2d7d4fe8a
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-25 21:37:13 +0800

    Add a simple tool to compare blob list
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

---
+++ findCommonBlobs.cpp
@@ -0,0 +1,67 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<string, string> filesMap;
+
+void showHelp(void) {
+    cout << "findCommonBlobs - Accept two proprietary-files.txt to find common part" << endl;
+    cout << endl;
+    cout << "usage: ./findCommonBlobs <proprietary-files1> <proprietary-files2>" << endl;
+}
+
+string removeMisc(string s) {
+    int pos = s.find("|");
+    if (pos == -1) {
+        int pos2 = s.find("-");
+        if (pos2 == 0) {
+            return s.erase(pos2, 1);
+        }
+        return s;
+    }
+    return s.erase(pos, s.length() - pos);
+}
+
+int main(int argc, char* argv[]) {
+    int j = 0;
+    string s1;
+    FILE* file;
+
+    if (argc < 3) {
+        cout << "Incorrect usage. Make sure you have two pathes in the commandline parameters."
+             << endl;
+        showHelp();
+        return 1;
+    }
+
+    ifstream file1(argv[1]);
+    ifstream file2(argv[2]);
+    if (!file1.is_open() || !file2.is_open()) {
+        cout << "File incorrect. Please check your input" << endl;
+        return 1;
+    }
+
+    while (file1 >> s1) {
+        s1 = removeMisc(s1);
+        filesMap[s1] = "1";
+    }
+
+    while (file2 >> s1) {
+        s1 = removeMisc(s1);
+        for (const auto& fiterator : filesMap) {
+            if (fiterator.first == s1) {
+                cout << "duplicated: " << fiterator.first << endl;
+                j++;
+            }
+        }
+    }
+
+    cout << "duplicated count: " << j << endl;
+
+    file1.close();
+    file2.close();
+    return 0;
+}
